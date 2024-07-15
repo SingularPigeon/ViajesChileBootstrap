684edc4dfa56b25f9771cb934373b93ac65f8aad
Paloma <paloma.ditex@gmail.com>
2024-07-15 15:53:24 -0400

    First commit

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+//Activación Tooltips
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
