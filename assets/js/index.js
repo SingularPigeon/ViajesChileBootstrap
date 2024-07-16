a8d234a8fc3b406318589e6813851c21538de11e
Paloma <paloma.ditex@gmail.com>
2024-07-15 23:23:29 -0400

    (wip) Viajes Chile bootstrap

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,15 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+//Activacion modal
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+//Activación de alerta
+const alertList = document.querySelectorAll('.alert')
+const alerts = [...alertList].map(element => new bootstrap.Alert(element))
